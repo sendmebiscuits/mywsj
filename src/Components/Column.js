3e4a1e83b6bf81826e5b56cadcca51c9d829dc97
Thomas Lee <Thomas Lee>
2018-05-24 22:25:18 -0400

    Updates

--- src/Components/Column.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Column = () => {
-  return 
-}
-
-export default Column;
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
-// eslint-disable-next-line
-import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
